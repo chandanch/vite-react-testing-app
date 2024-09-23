383bc325f18be402e1e505796b25a0fd784bc075
chandanch <chandan.cec107@gmail.com>
2024-09-23 13:38:13 +0530

    check setup with blank test

--- vite-starter/src/App.test.jsx
+++ vite-starter/src/App.test.jsx
@@ -1,8 +1,13 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
+import { expect } from "vitest";
 
 test("App contains correct heading", () => {
   render(<App />);
   const headingElement = screen.getByText(/learn react/i);
   expect(headingElement).toBeInTheDocument();
 });
+
+test('blank test check', () => {
+  expect(true).toBe(true)
+})
