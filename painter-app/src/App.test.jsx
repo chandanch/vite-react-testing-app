10662c79a0eaa192d9833df3a4d57467957e7874
chandanch <chandan.cec107@gmail.com>
2024-09-23 21:03:55 +0530

    add other tests

--- painter-app/src/App.test.jsx
+++ painter-app/src/App.test.jsx
@@ -8,3 +8,14 @@ test("button has a default color", () => {
   const buttonElement = screen.getByRole("button", {name: /blue/i});
   expect(buttonElement).toHaveClass("red");
 })
+
+
+test("button has a default text", () => {
+})
+
+
+test("button color is changed on btn click", () => {
+})
+
+test("button text is changed on btn click", () => {
+})
