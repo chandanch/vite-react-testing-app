58c0eceec1209479024e39896f69b769e9c27cb9
chandanch <chandan.cec107@gmail.com>
2024-09-26 11:12:19 +0530

    build summary form UI

--- sundae-starter/src/pages/summary/SummaryForm.jsx
+++ sundae-starter/src/pages/summary/SummaryForm.jsx
@@ -1,11 +1,30 @@
-// import React from 'react';
+import { useState } from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
-function SummaryForm() {
+export default function SummaryForm() {
+	const [tcChecked, setTcChecked] = useState(false);
+
+	const checkboxLabel = (
+		<span>
+			I agree to
+			<span style={{ color: 'blue' }}> Terms and Conditions</span>
+		</span>
+	);
+
 	return (
-		<div>
-			<h1>Order Summary</h1>
-		</div>
+		<Form>
+			<Form.Group controlId="terms-and-conditions">
+				<Form.Check
+					type="checkbox"
+					checked={tcChecked}
+					onChange={(e) => setTcChecked(e.target.checked)}
+					label={checkboxLabel}
+				/>
+			</Form.Group>
+			<Button variant="primary" type="submit" disabled={!tcChecked}>
+				Confirm order
+			</Button>
+		</Form>
 	);
 }
-
-export default SummaryForm;
