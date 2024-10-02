58229ff437aaea75d3d40543406f8040d29b8f21
chandanch <chandan.cec107@gmail.com>
2024-10-02 07:26:49 +0530

    implement options comp

--- sundae-starter/src/pages/entry/Options.jsx
+++ sundae-starter/src/pages/entry/Options.jsx
@@ -1,5 +1,24 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import ScoopOption from './ScoopOption';
+
 function Options({ optionType }) {
-	return <div>Options {optionType}</div>;
+	const [items, setItems] = useState([]);
+
+	useEffect(() => {
+		axios
+			.get(`http://localhost:3030/${optionType}`)
+			.then((response) => setItems(response.data))
+			.catch((error) => console.log(error));
+	}, [optionType]);
+
+	let itemOptions = '';
+
+	if (optionType === 'scoops') {
+		itemOptions = items.map((item) => <ScoopOption key={item.name} />);
+	}
+
+	return <div> {itemOptions} </div>;
 }
 
 export default Options;
