efd7273119cd5e21d4041ab5df95df0276b5eb03
chandanch <chandan.cec107@gmail.com>
2024-10-04 13:33:20 +0530

    add test for toppings

--- sundae-starter/src/pages/entry/tests/Options.test.jsx
+++ sundae-starter/src/pages/entry/tests/Options.test.jsx
@@ -3,7 +3,7 @@ import { describe, expect, test } from 'vitest';
 import Options from '../Options';
 
 describe('Options test', () => {
-	test('each option must display image', async () => {
+	test('each option must display scoops image', async () => {
 		render(<Options optionType="scoops" />);
 
 		const scoopImages = await screen.findAllByRole('img', { name: /scoop$/i });
@@ -12,5 +12,18 @@ describe('Options test', () => {
 		// check if alt text exists
 		const altTexts = scoopImages.map((scoopImage) => scoopImage.alt);
 		expect(altTexts).toEqual(['Chocolate scoop', 'Vanilla scoop']);
+	});
+
+	test('each option must display toppings image', async () => {
+		render(<Options optionType="toppings" />);
+
+		const toppingImages = await screen.findAllByRole('img', {
+			name: /topping$/i,
+		});
+		expect(toppingImages).toHaveLength(3);
+
+		// check if alt text exists
+		const altTexts = toppingImages.map((scoopImage) => scoopImage.alt);
+		expect(altTexts).toEqual(['Cherries', 'Hot Fudge', 'M&Ms']);
 	});
 });
