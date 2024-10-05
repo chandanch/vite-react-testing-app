70b61377eddbc75d5aa3c37abfcdf4324866edc8
chandanch <chandan.cec107@gmail.com>
2024-10-05 20:07:02 +0530

    log error when fetching data

--- sundae-starter/src/pages/entry/Options.jsx
+++ sundae-starter/src/pages/entry/Options.jsx
@@ -2,23 +2,21 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 import Row from 'react-bootstrap/Row';
 import ScoopOption from './ScoopOption';
+import ToppingOption from './ToppingOption';
 
 export default function Options({ optionType }) {
 	const [items, setItems] = useState([]);
+	const [error, setError] = useState(false);
 
 	// optionType is 'scoops' or 'toppings
 	useEffect(() => {
 		axios
 			.get(`http://localhost:3030/${optionType}`)
 			.then((response) => setItems(response.data))
-			.catch((error) => {
-				// TODO: handle error response
-				console.log(error);
-			});
+			.catch((error) => setError(true));
 	}, [optionType]);
 
-	// TODO: replace `null` with ToppingOption when available
-	const ItemComponent = optionType === 'scoops' ? ScoopOption : null;
+	const ItemComponent = optionType === 'scoops' ? ScoopOption : ToppingOption;
 
 	const optionItems = items.map((item) => (
 		<ItemComponent
