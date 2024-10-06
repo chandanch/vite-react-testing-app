85702b89bf0bb08b808332e7ff3639fd5bb20a33
chandanch <chandan.cec107@gmail.com>
2024-10-06 11:13:02 +0530

    test error message

---
+++ sundae-starter/src/pages/entry/tests/OrderEntry.test.jsx
@@ -0,0 +1,28 @@
+import { describe, expect, test } from 'vitest';
+import { server } from '../../../mocks/server';
+import { http, HttpResponse } from 'msw';
+import { render, screen } from '@testing-library/react';
+import OrderEntry from '../OrderEntry';
+
+describe('Order Entry tests', () => {
+	test('must display alert if data fetch fails', async () => {
+		// mock error response by using reset handlers
+		server.resetHandlers([
+			http.get('http://localhost:3030/scoops', () => {
+				return new HttpResponse(null, { status: 500 });
+			}),
+
+			http.get('http://localhost:3030/toppings', () => {
+				return new HttpResponse(null, { status: 500 });
+			}),
+		]);
+
+		render(<OrderEntry />);
+
+		const alertElements = await screen.findAllByRole('alert', {
+			name: /An unexpected error occured$/i,
+		});
+
+		expect(alertElements).toHaveLength(2);
+	});
+});
